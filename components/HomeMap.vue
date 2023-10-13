331e2bfefafedec71c8b22b1edd159382da33b22
Carl Egge <carl.egge@web.de>
2023-10-13 21:19:11 +0200

    change controls of mapbox

--- components/HomeMap.vue
+++ components/HomeMap.vue
@@ -1,21 +1,22 @@
 <template>
     <div>
-        <MapboxMap map-id="{ID}" style="position: relative; height: 250px; width: 100%;" :options="{
+        <MapboxMap :map-id="mapId" style="position: relative; height: 250px; width: 100%;" :options="{
             style: 'mapbox://styles/mapbox/streets-v12', // style URL
             center: [10.044652150704563, 53.585858561767836], // starting position 53.585858561767836, 10.044652150704563
             zoom: 14 // starting zoom
         }">
             <MapboxDefaultMarker marker-id="marker1" :options="{}" :lnglat="[10.044652150704563, 53.585858561767836]">
             </MapboxDefaultMarker>
+            <MapboxNavigationControl position="top-left" />
         </MapboxMap>
     </div>
 </template>
   
-<script>
-export default {
-    name: "MapImage",
-    data() {
-        return {}
-    },
-};
+<script setup>
+// import mapboxgl from 'mapbox-gl'
+const mapId = 'theaterdeck-hamburg'
+useMapbox(mapId, (map) => {
+    // disable scroll zoom
+    map.scrollZoom.disable()
+})
 </script>
