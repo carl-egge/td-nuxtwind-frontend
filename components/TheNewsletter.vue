96d059840b01ce7eae5870cb14b19e18d3d4aadb
Carl Egge <carl.egge@digitalsalt.de>
2025-08-27 12:21:08 +0200

    feat(newsletter): add cta button to newsletter

--- components/TheNewsletter.vue
+++ components/TheNewsletter.vue
@@ -10,49 +10,79 @@
 				<div>
 					<h3 class="text-3xl">Newsletter bestellen.</h3>
 					<p class="mt-6 leading-7">
-						Verpassen Sie keine unserer kommenden Theaterstücke und bleiben Sie
-						immer informiert! Melden Sie sich für unseren Newsletter an.
+						Verpasse keines unserer kommenden Theaterstücke und bleibe immer
+						informiert! Melde dich für unseren Newsletter an.
 					</p>
 					<UForm
 						:schema="schema"
 						:state="state"
-						aria-label="Newsletter Subscription"
-						class="mt-8 space-y-4"
+						aria-label="Newsletter Abonnieren"
+						class="mt-8 flex space-x-8"
 						role="form"
 						@submit="onSubmit"
 					>
-						<UFormGroup label="Name" name="name" class="label-white" required>
-							<UInput
-								v-model="state.name"
-								highlight
-								placeholder="Dein Name"
-								aria-label="Name"
-								:ui="{ rounded: 'rounded-none' }"
-								:disabled="waiting"
-							/>
-						</UFormGroup>
-						<UFormGroup
-							label="Email Adresse"
-							name="email"
-							class="label-white"
-							required
-						>
-							<UInput
-								v-model="state.email"
-								type="email"
-								placeholder="Deine Email-Adresse"
-								aria-label="Email"
-								:ui="{ rounded: 'rounded-none' }"
-								:disabled="waiting"
-							/>
-						</UFormGroup>
-						<UButton
+						<div class="flex-grow space-y-4">
+							<UFormGroup label="Name" name="name" class="label-white" required>
+								<UInput
+									v-model="state.name"
+									highlight
+									placeholder="Dein Name"
+									aria-label="Name"
+									:ui="{ rounded: 'rounded-none' }"
+									:disabled="waiting"
+								/>
+							</UFormGroup>
+							<UFormGroup
+								label="Email Adresse"
+								name="email"
+								class="label-white"
+								required
+							>
+								<UInput
+									v-model="state.email"
+									type="email"
+									placeholder="Deine Email-Adresse"
+									aria-label="Email"
+									:ui="{ rounded: 'rounded-none' }"
+									:disabled="waiting"
+								/>
+							</UFormGroup>
+						</div>
+						<!-- <UButton
 							type="submit"
 							aria-label="Subscribe"
 							:loading="waiting"
 							class="bg-primary-600 hover:bg-primary-400 flex-none px-4"
 						>
 							Ahoi und Enter!
+						</UButton> -->
+						<UButton
+							type="submit"
+							aria-label="Abonnieren"
+							:loading="waiting"
+							variant="link"
+							class="group inline-block h-32 w-32 -rotate-6 transition-transform duration-300 hover:rotate-0 hover:scale-105 md:h-40 md:w-40"
+						>
+							<svg
+								viewBox="82 96 314 293"
+								xmlns="http://www.w3.org/2000/svg"
+								class="pointer-events-none h-full w-full drop-shadow-lg transition-transform duration-300 group-hover:scale-105"
+							>
+								<path
+									fill="#f51663"
+									class="pointer-events-auto"
+									d="M395.712,240 Q368.5593873417986,305.60819706018026 362.9980088455405,319.04602055443405 Q306.6236043716255,353.5373956448795 295.0770395998598,360.60189415738034 Q231.54048958776613,384.4612774860368 218.52697934432527,389.3481391316652 Q155.9583522768417,358.1656136442555 143.14309131121254,351.7788313155089 Q109.4718535422772,292.4941649471893 102.57533496309767,280.3515224380154 Q85.82579700063415,208.44988710065365 82.3951687432621,193.72304612794102 Q129.61592592324243,135.6634695746937 139.28764727335889,123.7717490758358 Q205.80172882881897,101.46686268394083 219.4250949305397,96.89839197716717 Q290.70952600611963,96.97538442874782 305.3099516481059,96.99115396702337 Q349.3054720253195,152.5774014614045 358.31660270498975,163.96253649037413 Q389.35478245984825,227.0736312033636 395.712,240 Z"
+								/>
+							</svg>
+							<div
+								class="pointer-events-none absolute inset-0 flex flex-col items-center justify-center text-xl font-semibold uppercase text-background group-hover:text-theme-accent"
+							>
+								<span>Ahoi</span>
+								<span class="flex items-start">
+									<span class="text-4xl">&amp;</span>
+									Enter
+								</span>
+							</div>
 						</UButton>
 
 						<!-- Accessible feedback -->
